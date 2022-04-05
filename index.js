99f30ca485ebe3529f7d02841c0c3614f570b2ec
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-05 13:38:33 -0400

    commit tree 752468d275758567f68aa4e23f94a0364f018727

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]
+
+let destructivelyAppendCat = (cat) => {
+    cats.push(cat);
+}
+
+let destructivelyPrependCat = (cat) => {
+    cats.unshift(cat)
+}
+
+let destructivelyRemoveLastCat = () => {
+    cats.pop()
+}
+
+let destructivelyRemoveFirstCat = () => {
+    cats.shift();
+}
+
+let appendCat = (name) => {
+    return [...cats, name]
+}
+
+let prependCat = (name) => {
+    return [name, ...cats]
+}
+
+let removeLastCat = () => {
+    return cats.slice(0, cats.length - 1);
+}
+
+let removeFirstCat = () => {
+    return cats.slice(1, cats.length);
+}
